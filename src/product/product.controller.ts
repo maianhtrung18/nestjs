0a6f4403d7d832f6904621bccb980d86b1fcec1c
TrungMai <anh.maitrung@gmail.com>
2025-01-14 19:35:00 +0700

    push

---
+++ src/product/product.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  Query,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('product/:uid')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  getDemo(@Req() req, @Query('uname') uname, @Param('uid') uid, @Body() body) {
+    // let { uname } = req.query;
+    // let { uid } = req.params;
+    return { uname, uid, body };
+  }
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productService.remove(+id);
+  }
+}
